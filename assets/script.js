ea2fd78eeb6c835147e32de6056992bb320e2d46
Dave Strider <evan.ebert17@gmail.com>
2024-02-20 01:57:32 -0600

    added the complete 'fetchNextPage' function inside of searchForPlaylist  which is a recursive call for pagination (checking to see if there are any pages left to get information from

--- assets/script.js
+++ assets/script.js
@@ -2,41 +2,78 @@
 //test url https://www.youtube.com/playlist?list=PL2uxd6YWj7PKk4LnkWZEyqpcvnXmv8Iuf
 
 //this generates our video by taking the url the user passes, stripping it of just our useable url, and then generating an iframe.
-document.getElementById("generatePlaylist").addEventListener("click", function() {
+document.getElementById("generatePlaylist").addEventListener("click", function () {
     var userInputtedURL = document.getElementById("videoURL").value
 
     var urlFormatting = (/https:\/\/www\.youtube\.com\/playlist\?list=/);
 
-    var formattedURL = userInputtedURL.replace(urlFormatting,"")
+    var formattedURL = userInputtedURL.replace(urlFormatting, "")
     console.log(formattedURL)
 
+    let arrayOfAllVideos = [];
+    searchForPlaylist(formattedURL,arrayOfAllVideos);
     // playlistTypeSelector()
+
 });
 
- function searchForPlaylist(urlOfPlaylist) {
+async function searchForPlaylist(urlOfPlaylist,videoItems) {
 
     //this line is taking the "youtube.googleapis" api and fetiching all the formation in a playlist 
     //currently set to limit of 10 items, change &maxResults=10 to change this.
     //to change the url of the playlist retrieved, change the &playldistId= 's url.
     //last part is the API key, but you don't need to change that.
+
+    let returnedANextPage = true;
+
+    // var playlistItems = data.items;
+    // returnedANextPage = data.nextPageToken;
+
+    //this will hold the value of 1 video for our users, as an example.
+    // `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${urlOfPlaylist}&key=AIzaSyAD7JowNHoI4KsaRB_eLKUMRsDhzNv5opw`
+
+    let url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${urlOfPlaylist}&key=AIzaSyAD7JowNHoI4KsaRB_eLKUMRsDhzNv5opw`
+
+    //initializing next page so it doesn't get flagged at start of recursion.
+    let nextPage = 'h';
+
+
+    function fetchNextPage() {
+        //while there IS a next page to get information from...
+        if (!nextPage) return;
+
+        //our url we declared above will be changed as we recurse.
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            //json object that gives us our data back
+            console.log(data);
+
+            //nextPage is grabbed to get our token that tells us if we have a next page to go to
+            nextPage = data.nextPageToken;
+
+            //url gets updated with nextpage, should it have a next page
+            url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken=${nextPage}&playlistId=${urlOfPlaylist}&key=AIzaSyAD7JowNHoI4KsaRB_eLKUMRsDhzNv5opw`;
+
+            //we take all the data items we were given (should be ~50 per page unless page has >50 and we're about to stop recursing)
+            for(i = 0; i < 50; i++) {
+                //this is getting the unique id/url of the video at index i.
+                let individualVideo = data.items[i].snippet.resourceId.videoId
+
+                //sending them to our array up in the button eventListener
+                videoItems.push(individualVideo);
+            }
+            // Call the function recursively for the next page, should it exist
+            fetchNextPage();
+        })
+
+        //error handling
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }
     
-
-    // playlistTypeSelector();
-
-    fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=${urlOfPlaylist}&key=AIzaSyAD7JowNHoI4KsaRB_eLKUMRsDhzNv5opw`)
-    .then(function (response) {
-        //this is just getting back the object in JSON 
-        return response.json();
-    })
-    //this is our readable api data back
-    .then(data => {
-        //look through this to get your information you want.
-        console.log(data)
-
-        var playlistItems = data.items;
-        
-        putVideosInPlaylist(playlistItems, whatShuffleToUse)
-    })
+    //initial self-call
+    fetchNextPage();
 
 }
 
@@ -54,14 +91,14 @@ function putVideosInPlaylist(playlistItemsToShuffle) {
     const iframeVideoLocation = `<iframe id="videoCurrentlyPlaying" width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/${singleVideoInformation}" frameborder="0" allow="encrypted-media" allowfullscreen=""></iframe>`
     var videoLocation = document.getElementById("videoLocation");
     videoLocation.innerHTML = iframeVideoLocation;
-    
+
 }
 
- function playlistTypeSelector() {
+function playlistTypeSelector() {
     //this function returns the choice a user made in their preferred shuffle method and generates the card to make that choice.
     var mcvhInsertingChoiceCard = document.getElementById('middleContentVideoHolder')
     var floatingChoiceDiv = document.createElement('div');
-    floatingChoiceDiv.setAttribute('id','floatingChoiceDiv');
+    floatingChoiceDiv.setAttribute('id', 'floatingChoiceDiv');
 
     //this card creates 3 buttons that will determine the "3" choices the user can make.
     floatingChoiceDiv.innerHTML = `
@@ -83,6 +120,6 @@ function putVideosInPlaylist(playlistItemsToShuffle) {
     // dimWebpage.style.display = "block"
 
     var trueRandomButton = document.getElementById("trueRandom")
-    
+
 }
 
