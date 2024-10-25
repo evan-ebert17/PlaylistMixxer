3a729d009e25fc6324456216d6ce2114a61a103c
Dave Strider <evan.ebert17@gmail.com>
2024-10-25 15:17:43 -0500

    reverting sending  over in initial request

--- assets/script.js
+++ assets/script.js
@@ -34,7 +34,8 @@ document.getElementById("generatePlaylist").addEventListener("click", function (
     //to change the url of the playlist retrieved, change the &playldistId= 's url.
     //last part is the API key, but you don't need to change that.
 
-    const apiUrl = `https://ikrh3hyhzc.execute-api.us-east-2.amazonaws.com/getAPIKEY?playlistId=${formattedPlaylistID}?pageToken=${next_pageToken}`;
+    const apiUrl = `https://ikrh3hyhzc.execute-api.us-east-2.amazonaws.com/getAPIKEY?playlistId=${formattedPlaylistID}`;
+    //?pageToken=${next_pageToken}
 
     //we fetch the url
     fetch(apiUrl)
